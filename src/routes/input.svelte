36df7729ec72bd056185656dd0d2f88ec718d1ec
toooooby64 <jalvarez5667@gmail.com>
2023-12-23 12:03:28 -0800

    Added signup page and input component

---
+++ src/routes/input.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let type;
+	export let placeholder;
+	export let label;
+</script>
+
+<label class="form-control w-full max-w-xs">
+	<div class="label">
+		<span class="label-text">{label}</span>
+	</div>
+	<input {type} {placeholder} class="input input-bordered w-full max-w-xs" />
+</label>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Input from '../input.svelte';
+</script>
+
+<div>
+	<Input type="text" placeholder="Enter username here" label="Username" />
+	<Input type="password" placeholder="Enter password here" label="Password" />
+</div>
